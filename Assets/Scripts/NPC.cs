bd12f885744529380307e0095da44f45e086a43e
Diegolo03 <diego.atila099@gmail.com>
2024-12-04 21:20:20 +0100

    interactuar npc

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,18 +1,35 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Outline outline;
+    [SerializeField] private Texture2D cursorInteraccion;
+    [SerializeField] private Texture2D cursorPorDefecto;
+    [SerializeField] private float tiemporotacion;
+    public void Interactuar(Transform interactuador)
     {
-        
+        Debug.Log("Hola");
+        transform.DOLookAt(interactuador.position,tiemporotacion,AxisConstraint.Y);
+    }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+    private void OnMouseEnter()
+    {
+
+        Cursor.SetCursor(cursorInteraccion, Vector2.zero, CursorMode.Auto);
+        outline.enabled = true;
+
+    }
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorPorDefecto, Vector2.zero, CursorMode.Auto);
+        outline.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Camera cam;
+    private NPC npcActual;
+    [SerializeField]private float distanciaInteraccion;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +23,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        InteractuarYMover();
         
-       Ray ray= cam.ScreenPointToRay(Input.mousePosition);
+        if (npcActual)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+
+                npcActual.Interactuar(this.transform);
+                npcActual = null;
+                
+                agent.isStopped = true;
+                agent.stoppingDistance = 0;
+            }
+        }
+
+    }
+
+    private void InteractuarYMover()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
 
             if (Input.GetMouseButtonDown(0))
             {
+                if (hit.transform.TryGetComponent(out NPC npc))
+                {
+                    npcActual = npc;
+                    agent.stoppingDistance = distanciaInteraccion;
+                }
 
-                 agent.SetDestination(hit.point);
+                agent.SetDestination(hit.point);
 
             }
 
         }
-;
     }
 }
